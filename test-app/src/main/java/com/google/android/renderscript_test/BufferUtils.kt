17fda9ea4db08b0c2eacd202e837389fc6570fab
fvito <fvito@users.noreply.github.com>
2025-07-28 11:05:13 +0200

    Update dependencies

--- test-app/src/main/java/com/google/android/renderscript_test/BufferUtils.kt
+++ test-app/src/main/java/com/google/android/renderscript_test/BufferUtils.kt
@@ -28,6 +28,7 @@ import java.util.Random
 import kotlin.math.floor
 import kotlin.math.max
 import kotlin.math.min
+import androidx.core.graphics.createBitmap
 
 /**
  * A vector of 4 integers.
@@ -480,7 +481,8 @@ fun vectorSizeOfBitmap(bitmap: Bitmap): Int {
 }
 
 fun duplicateBitmap(original: Bitmap): Bitmap {
-    val copy = Bitmap.createBitmap(original.width, original.height, original.config)
+    val copy =
+        createBitmap(original.width, original.height, original.config ?: Bitmap.Config.ARGB_8888)
     val canvas = Canvas(copy)
     canvas.drawBitmap(original, 0f, 0f, null)
     return copy
